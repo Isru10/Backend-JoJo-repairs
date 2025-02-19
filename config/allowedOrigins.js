1a5c0e05356c9dc726e89b84026e5abfe5f05924
Israel Sol <isrucrasus10@gmail.com>
2025-02-20 00:58:23 +0300

    again handled domain name cors issue

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -4,7 +4,8 @@ const allowedOrigins = [
     // 'https://dandrepairshop.com',
     // 'http://localhost:5173',
     'https://technotes-6d2k.onrender.com',
-    'https://technotes-api-hf2v.onrender.com/'
+    // 'https://technotes-api-hf2v.onrender.com',
+    'http://localhost:5173'
 ];
 
 module.exports = allowedOrigins;
